17940c6dece3278fb5a624182ac63f8091b4fc6f
M-Muiz <mmuizsiddiqui@gmail.com>
2025-11-27 23:47:09 +0500

    work on multiple data types

--- first.cpp
+++ first.cpp
@@ -7,8 +7,6 @@
 //     return 0;
 // }
 
-
-
 // Method 2
 
 // #include <iostream>
@@ -19,3 +17,38 @@
 //     cout<<"Hy its me!";
 //     return 0;
 // }
+
+// #include<iostream>
+
+// int a = 10;
+
+// using namespace std;
+// int main(){
+//         cout<< a;
+//         return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 23;
+    // int b = 35;
+
+    int a = 12, b = 88; // Recommended way to create multiple variable of same type:
+
+    float pi = 3.14;
+
+    char firstletter = 'M';
+
+    // double name = 'Moiz';
+
+    cout << "The value of a is " << a << " and the value of b is " << b << " and the sum of both is " << a + b;
+
+    cout << "\nThe value of pi is " << pi;
+
+    cout << "\nThis code is created by " <<firstletter;
+
+    return 0;
+}
