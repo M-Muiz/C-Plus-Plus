a6263d01440170ecde4f224168924c54bf7fe026
M-Muiz <mmuizsiddiqui@gmail.com>
2025-11-27 23:12:26 +0500

    work on 2 methods

--- first.cpp
+++ first.cpp
@@ -1,6 +1,21 @@
-#include<iostream>
+// Method 1
 
-int main(){
-    std::cout<<"Hello World";
-    return 0;
-}
+// #include<iostream>
+
+// int main(){
+//     std::cout<<"Hello World";
+//     return 0;
+// }
+
+
+
+// Method 2
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+//     cout<<"Hy its me!";
+//     return 0;
+// }
