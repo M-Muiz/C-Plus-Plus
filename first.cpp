5e48ed4cff0fec2ce5fb2001e50b6255b7b9bbd2
M-Muiz <mmuizsiddiqui@gmail.com>
2025-11-27 23:55:32 +0500

    work on globat and local  scopes.

--- first.cpp
+++ first.cpp
@@ -38,17 +38,19 @@ int main()
 
     int a = 12, b = 88; // Recommended way to create multiple variable of same type:
 
+    bool is_true = true;
+
     float pi = 3.14;
 
     char firstletter = 'M';
 
     // double name = 'Moiz';
 
-    cout << "The value of a is " << a << " and the value of b is " << b << " and the sum of both is " << a + b;
+    cout << "The value of a is " << a << " and the value of b is " << b << " and the sum of both is " << a + b + is_true;
 
     cout << "\nThe value of pi is " << pi;
 
-    cout << "\nThis code is created by " <<firstletter;
+    cout << "\nThis code is created by " << firstletter;
 
     return 0;
 }
