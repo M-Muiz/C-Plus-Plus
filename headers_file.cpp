e1f3917c62dba39f5aba768744b5a528f8c77bf0
M-Muiz <mmuizsiddiqui@gmail.com>
2025-11-28 23:21:44 +0500

    access golbal variable in private scope

--- headers_file.cpp
+++ headers_file.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+// int main()
+// {
+//     cout<<"Leaning Header File.";
+//     return 0;
+// }
+
+
+int c = 35;
+
+
 int main()
 {
-    cout<<"Leaning Header File.";
+    int a, b, c;
+    cout << "Enter the value of a: " << endl;
+    cin >> a;
+
+    cout << "Enter the value of b: " << endl;
+    cin >> b;
+
+    c = a + b;
+    cout << "The sum of a and b is: " << c <<endl;
+
+    cout<<"The value of global c is "<<::c;
+
     return 0;
 }
