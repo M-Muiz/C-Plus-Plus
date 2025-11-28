50a49ffca8907f3ed671c932239002a54687ee55
M-Muiz <mmuizsiddiqui@gmail.com>
2025-11-28 23:47:16 +0500

    learn some type casting

--- headers_file.cpp
+++ headers_file.cpp
@@ -29,14 +29,25 @@ using namespace std;
 
 int main()
 {
-    float y = 24.5;
-    long double z = 24.5;
+    // float y = 24.5;
+    // long double z = 24.5;
 
-    cout << "The size of 24.5 is " << sizeof(24.4)<<endl;
-    cout << "The size of 24.5f is " << sizeof(24.4f)<<endl;
-    cout << "The size of 24.5F is " << sizeof(24.4F)<<endl;
-    cout << "The size of 24.5l is " << sizeof(24.4l)<<endl;
-    cout << "The size of 24.5L is " << sizeof(24.4L)<<endl;
+    // cout << "The size of 24.5 is " << sizeof(24.4)<<endl;
+    // cout << "The size of 24.5f is " << sizeof(24.4f)<<endl;
+    // cout << "The size of 24.5F is " << sizeof(24.4F)<<endl;
+    // cout << "The size of 24.5l is " << sizeof(24.4l)<<endl;
+    // cout << "The size of 24.5L is " << sizeof(24.4L)<<endl;
+
+    // float ab = 343;
+    // float &cd = ab;
+
+    // cout << ab << endl;
+    // cout << cd << endl;
+
+    float num = 34.534;
+
+    cout << "The value of float change in int is :" << (int)num;
+    cout << "The value of float change in int is :" << int(num);
 
     return 0;
 }
