9d21f3991919870c4a2144eb7f4ca85e777af38d
M-Muiz <mmuizsiddiqui@gmail.com>
2025-11-28 23:34:27 +0500

    work on literals

--- headers_file.cpp
+++ headers_file.cpp
@@ -8,23 +8,35 @@ using namespace std;
 //     return 0;
 // }
 
+// int c = 35;
 
-int c = 35;
+// int main()
+// {
+//     int a, b, c;
+//     cout << "Enter the value of a: " << endl;
+//     cin >> a;
 
+//     cout << "Enter the value of b: " << endl;
+//     cin >> b;
+
+//     c = a + b;
+//     cout << "The sum of a and b is: " << c <<endl;
+
+//     cout<<"The value of global c is "<<::c;
+
+//     return 0;
+// }
 
 int main()
 {
-    int a, b, c;
-    cout << "Enter the value of a: " << endl;
-    cin >> a;
+    float y = 24.5;
+    long double z = 24.5;
 
-    cout << "Enter the value of b: " << endl;
-    cin >> b;
-
-    c = a + b;
-    cout << "The sum of a and b is: " << c <<endl;
-
-    cout<<"The value of global c is "<<::c;
+    cout << "The size of 24.5 is " << sizeof(24.4)<<endl;
+    cout << "The size of 24.5f is " << sizeof(24.4f)<<endl;
+    cout << "The size of 24.5F is " << sizeof(24.4F)<<endl;
+    cout << "The size of 24.5l is " << sizeof(24.4l)<<endl;
+    cout << "The size of 24.5L is " << sizeof(24.4L)<<endl;
 
     return 0;
 }
